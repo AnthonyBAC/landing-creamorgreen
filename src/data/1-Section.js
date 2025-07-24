2593c2a01b50afbe4b1b72decdda6c543c35ea3d
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-07-24 15:48:51 -0400

    section 1 ok

---
+++ src/data/1-Section.js
@@ -0,0 +1,9 @@
+export const content = [
+  {
+    title: 'Bienvenido a Creamorgreen',
+    subtitle:
+      'Crea espacios mas saludables y acogedores con nuestras plantas y consejos especializados',
+    image: '/slide_image-1.jpg',
+    image2: '/slide_image-2.jpg',
+  },
+];
